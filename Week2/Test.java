3650892af33ce7ee66e1125fabffd14692225a3f
lydiekippe <lydiekippe@gmail.com>
2020-07-17 17:41:12 -0400

    muester

---
+++ Week2/Test.java
@@ -0,0 +1,14 @@
+class Variables{
+int age = 29 ;
+static char my_initial = 'L';
+static String first_name = "Lydie ";
+    public static void main(String[] args) {
+int year = 2020 ;
+        System.out.println( my_initial);
+        System.out.println(year);
+        System.out.println(first_name);
+        Variables obj = new Variables();
+        System.out.println(obj.age);
+
+    }
+}
